559945a2f6282182d41da8e7bf9ec9e8e24fc23b
Diego Forero <gollum23@gmail.com>
2024-08-19 06:30:39 -0500

    refactor: Add username to script and action

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,10 @@
-print('Hello world!')
+import os
+
+
+def main():
+    name = os.getenv('USERNAME')
+    print(f'Hello {name} from GitHub Actions!!! :)')
+
+
+if __name__ == '__main__':
+    main()
